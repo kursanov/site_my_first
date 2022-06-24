58fb6a11207f58844b5950e159a71831baa0665f
kursanov <z.kursanov@yandex.ru>
2022-06-25 02:11:29 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import javax.lang.model.element.Name;
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] array = {"sagyn", "marat", "dosbol"};
+        
+        for (String a : array) {
+            System.out.println("I Love You" + "  " +a);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
